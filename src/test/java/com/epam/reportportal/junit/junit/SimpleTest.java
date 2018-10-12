9492b59745e6ab8ee5b667442fb9d98eecb97d76
Scott Babcock <scoba@hotmail.com>
2018-10-12 13:15:01 -0700

    Remove more non-related changes

--- src/test/java/com/epam/reportportal/junit/junit/SimpleTest.java
+++ src/test/java/com/epam/reportportal/junit/junit/SimpleTest.java
@@ -3,16 +3,11 @@ package com.epam.reportportal.junit.junit;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SimpleTest {
 	
-	private static Logger LOGGER = LoggerFactory.getLogger(SimpleTest.class);
-	
 	@Test
 	public void passingTest() {
-		LOGGER.info("This is a message from a passing test");
 		assertTrue("This test shouldn't fail", true);
 	}
 	
