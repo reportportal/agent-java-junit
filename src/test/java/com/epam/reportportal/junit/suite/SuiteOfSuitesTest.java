a97176ad3e295303306bf127063dc0ae37115eda
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-11-09 16:40:02 +0300

    A suite of suites test added

--- src/test/java/com/epam/reportportal/junit/suite/SuiteOfSuitesTest.java
+++ src/test/java/com/epam/reportportal/junit/suite/SuiteOfSuitesTest.java
@@ -18,6 +18,7 @@ package com.epam.reportportal.junit.suite;
 
 import com.epam.reportportal.junit.ReportPortalListener;
 import com.epam.reportportal.junit.features.suites.SimpleSuiteClass;
+import com.epam.reportportal.junit.features.suites.SuiteOfSuitesClass;
 import com.epam.reportportal.junit.features.suites.tests.FirstSuiteTest;
 import com.epam.reportportal.junit.features.suites.tests.SecondSuiteTest;
 import com.epam.reportportal.junit.utils.TestUtils;
@@ -34,9 +35,7 @@ import org.mockito.ArgumentMatchers;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -45,25 +44,27 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.*;
 
-public class SimpleSuiteTest {
+public class SuiteOfSuitesTest {
 	private static final int TEST_NUMBER = 2;
 
-	private final String suiteId = CommonUtils.namedId("suite_");
+	private final String firstSuiteId = CommonUtils.namedId("suite1_");
+	private final String secondSuiteId = CommonUtils.namedId("suite2_");
 	private final List<String> classIds = Stream.generate(() -> CommonUtils.namedId("class_"))
 			.limit(TEST_NUMBER)
 			.collect(Collectors.toList());
 	private final List<String> methodIds = Stream.generate(() -> CommonUtils.namedId("method_"))
 			.limit(TEST_NUMBER)
 			.collect(Collectors.toList());
-	private final List<Pair<String, Set<String>>> tests = IntStream.range(0, TEST_NUMBER)
-			.mapToObj(i -> Pair.of(classIds.get(i), Collections.singleton(methodIds.get(i))))
+	private final List<Pair<String, String>> tests = IntStream.range(0, TEST_NUMBER)
+			.mapToObj(i -> Pair.of(classIds.get(i), methodIds.get(i)))
 			.collect(Collectors.toList());
 
 	private final ReportPortalClient client = mock(ReportPortalClient.class);
 
 	@BeforeEach
 	public void setupMock() {
-		TestUtils.mockLaunch(client, null, suiteId, tests);
+		TestUtils.mockLaunch(client, null, firstSuiteId, secondSuiteId, classIds);
+		TestUtils.mockNestedSteps(client, tests);
 		TestUtils.mockLogging(client);
 		ReportPortalListener.setReportPortal(ReportPortal.create(client, TestUtils.standardParameters()));
 	}
@@ -76,36 +77,41 @@ public class SimpleSuiteTest {
 				.orElse("");
 	}
 
+	private static final List<Class<?>> SUITE_NAMES = Arrays.asList(SuiteOfSuitesClass.class, SimpleSuiteClass.class);
 	private static final List<Class<?>> TEST_NAMES = Arrays.asList(FirstSuiteTest.class, SecondSuiteTest.class);
 	private static final List<String> STEP_NAMES = Arrays.asList(getMethodName(TEST_NAMES.get(0)), getMethodName(TEST_NAMES.get(1)));
 
 	@Test
-	public void verify_test_hierarchy_on_simple_suite() {
-		TestUtils.runClasses(SimpleSuiteClass.class);
+	public void verify_test_hierarchy_on_suite_of_suites() {
+		TestUtils.runClasses(SuiteOfSuitesClass.class);
 
 		ArgumentCaptor<StartTestItemRQ> suiteCaptor = ArgumentCaptor.forClass(StartTestItemRQ.class);
 		verify(client, times(1)).startTestItem(ArgumentMatchers.startsWith(TestUtils.ROOT_SUITE_PREFIX), suiteCaptor.capture());
+		verify(client, times(1)).startTestItem(same(firstSuiteId), suiteCaptor.capture());
 		ArgumentCaptor<StartTestItemRQ> testCaptor = ArgumentCaptor.forClass(StartTestItemRQ.class);
-		verify(client, times(2)).startTestItem(same(suiteId), testCaptor.capture());
+		verify(client, times(2)).startTestItem(same(secondSuiteId), testCaptor.capture());
 		ArgumentCaptor<StartTestItemRQ> stepCaptor = ArgumentCaptor.forClass(StartTestItemRQ.class);
 		verify(client, times(1)).startTestItem(same(classIds.get(0)), stepCaptor.capture());
 		verify(client, times(1)).startTestItem(same(classIds.get(1)), stepCaptor.capture());
 
-		StartTestItemRQ suite = suiteCaptor.getValue();
-		String suiteCodeRef = SimpleSuiteClass.class.getCanonicalName();
-		assertThat(suite.getName(), equalTo(suiteCodeRef));
-		assertThat(suite.getCodeRef(), equalTo(suiteCodeRef));
-		assertThat(suite.getType(), equalTo(ItemType.SUITE.name()));
+		List<StartTestItemRQ> suites = suiteCaptor.getAllValues();
+		IntStream.range(0, tests.size()).forEach(i -> {
+			String suiteCodeRef = SUITE_NAMES.get(i).getCanonicalName();
+			StartTestItemRQ suite = suites.get(i);
+			assertThat(suite.getName(), equalTo(suiteCodeRef));
+			assertThat(suite.getCodeRef(), equalTo(suiteCodeRef));
+			assertThat(suite.getType(), equalTo(ItemType.SUITE.name()));
+		});
 
 		List<StartTestItemRQ> tests = testCaptor.getAllValues();
-		IntStream.range(0, tests.size()).forEach(i->{
+		IntStream.range(0, tests.size()).forEach(i -> {
 			StartTestItemRQ test = tests.get(i);
 			assertThat(test.getName(), equalTo(TEST_NAMES.get(i).getCanonicalName()));
 			assertThat(test.getType(), equalTo(ItemType.TEST.name()));
 		});
 
 		List<StartTestItemRQ> steps = stepCaptor.getAllValues();
-		IntStream.range(0, steps.size()).forEach(i->{
+		IntStream.range(0, steps.size()).forEach(i -> {
 			StartTestItemRQ step = steps.get(i);
 			assertThat(step.getName(), equalTo(STEP_NAMES.get(i)));
 			assertThat(step.getType(), equalTo(ItemType.STEP.name()));
